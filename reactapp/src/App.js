46d89b3eb5dc9e40909811cb3eed85337322a12a
Alperen Kilinc <kilincfurkanalperen@gmail.com>
2022-07-11 01:20:21 +0300

    components

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import User from './components/User';
 import './App.css';
 
-function App() {
- const test = 22;
- const alfa = true;
-  return (
-    <div className="App">
-      <h1>{"alperen".toUpperCase()}</h1>
-      <p>Zort</p>
-      <h2>{test}</h2>
-      <h5>{alfa ? <p>Güneşlerr</p>
-      : <p>yanlışşş</p>}</h5>
-    </div>
-  );
+class App extends Component{
+
+  render(){
+    return(
+      <div className='App'>
+        <h4>App Component</h4>
+        <User/>
+        <User/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
